d80c7ba707e921f7afa32d5bdd38023324419029
Mat Withers <github@m4t.uk>
2021-03-16 16:46:18 +0000

    added support files from original project

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
+
+VID = 0x1b4f      #USB Vendor ID for a Pro Micro
+PID = 0x9206      #USB Product ID for a Pro Micro
+
+DEBUG = True
+
+from inkkeys import *        #Inkkeys module
+from modes import *  
+
 import PySimpleGUIWx as sg
+import PySimpleGUI as psg
 import json
 from pathlib import Path
 import pprint
+import os
+
+from serial import SerialException  #Serial functions
+import serial.tools.list_ports      #Function to iterate over serial ports
+import traceback                    #Print tracebacks if an error is thrown and caught
 
 import win32process
 import win32api
@@ -35,7 +50,13 @@ def saveConfig(newConfig):
   return
 
 def initConfig():
-  newConfig = {'first': 'aaabbbbxx', 'second': 'like horses', 'third': True}
+
+  newConfig = {
+    'first': 'aaabbbbxx',
+    'second': 'like horses',
+    'third': True
+  }
+
   print("Config initialized . . .")
   pprint.pprint(newConfig)
   print("-------------------")
@@ -43,28 +64,59 @@ def initConfig():
 
 def settingsWindow(config):
     # All the stuff inside your window.
+    # settingsLayout = [
+    #   [sg.Text('First setting'), sg.InputText(key='first', default_text=config['first'])],
+    #   [sg.Text('Second setting'), sg.InputCombo(('like biscuits', 'like horses'), size=(20, 1), key='second', default_value=config['second'])],
+    #   [sg.Checkbox('Checkbox!!!', default=config['third'], key='third')],
+    #   [sg.Button('Ok'), sg.Button('Cancel')]
+    # ]    
+    # Column layout      
+    col = [[psg.Text('col Row 1')],
+           [psg.Text('col Row 2'),psg.Input('col input 1')],
+           [psg.Text('col Row 3'), psg.Input('col input 2')]]
+
     settingsLayout = [
-      [sg.Text('First setting'), sg.InputText(key='first', default_text=config['first'])],
-      [sg.Text('Second setting'), sg.InputCombo(('like biscuits', 'like horses'), size=(20, 1), key='second', default_value=config['second'])],
-      [sg.Checkbox('Checkbox!!!', default=config['third'], key='third')],
-      [sg.Button('Ok'), sg.Button('Cancel')]
-    ]    
+      [
+        psg.Listbox(values=('Code.exe', 'thunderbird.exe', 'msedge.exe', 'Discord.exe', 'WhatsApp.exe'), select_mode=psg.LISTBOX_SELECT_MODE_SINGLE , size=(20,20), enable_events=True, key='_APPLICATION_'),
+        psg.Column(col)
+      ],
+      [
+        psg.Button('Ok'),
+        psg.Button('Cancel')
+      ]
+    ]
 
     #open window
-    settingsWindow = sg.Window('Macro Keyboard Settings', settingsLayout)    
+    settingsWindow = psg.Window('Macro Keyboard Settings', settingsLayout)    
 
-    # read from window on event
-    settingsEvent, newConfig = settingsWindow.read()    
+    loop = True
+    while loop:
+      # read from window on event
+      settingsEvent, newConfig = settingsWindow.read()    
 
-    if settingsEvent == sg.WIN_CLOSED or settingsEvent == 'Cancel':
+      pprint.pprint(settingsEvent)
+
+      if settingsEvent == '_APPLICATION_':
+        print(newConfig)
+
+      if settingsEvent == psg.WIN_CLOSED or settingsEvent == 'Cancel':
+        loop = False
         settingsWindow.close()
-    if settingsEvent == 'Ok':
+      if settingsEvent == 'Ok':
+        loop = False
         saveConfig(newConfig)
         settingsWindow.close()
 
+
+
 # called when window change event occurs
 def handleWindowChange(exePath):
-  print(exePath)
+  if exePath in config["applications"]:
+    pprint.pprint(config["applications"][exePath])
+    mode = ModeApplication()
+    mode.activate(device, exePath, config["applications"][exePath])
+  else:
+    print("Application not recognized")
 
 # main handler to trigger changes to current active window
 # largely copied from:
@@ -96,7 +148,8 @@ def windowChangeEventListener():
   def callback(hWinEventHook, event, hwnd, idObject, idChild, dwEventThread, dwmsEventTime):
     _, pid = win32process.GetWindowThreadProcessId(hwnd)
     handle = win32api.OpenProcess(win32con.PROCESS_QUERY_INFORMATION|win32con.PROCESS_VM_READ,0,pid)
-    exePath = win32process.GetModuleFileNameEx(handle, 0)
+    #exePath = win32process.GetModuleFileNameEx(handle, 0)
+    head, exePath = os.path.split(win32process.GetModuleFileNameEx(handle, 0))
     handleWindowChange(exePath)
 
   WinEventProc = WinEventProcType(callback)
@@ -126,28 +179,60 @@ def windowChangeEventListener():
   user32.UnhookWinEvent(hook)
   ole32.CoUninitialize()
 
+def work():
+  # Start the 'run' method in a daemonized thread.
+  t = threading.Thread(target=windowChangeEventListener)
+  t.setDaemon(True)
+  t.start()
+
+  # Event Loop to process "events"
+  while True:
+    menu_item = tray.read()
+    print(menu_item)
+    if menu_item == 'Exit':
+      tray.close()
+      break
+    elif menu_item == 'Settings':
+      settingsWindow(config)
+      config = loadConfig()
+      print("settings done")
+
+#Try connecting on the given port and work with it.
+#Will return false if connection fails or an unknown device is present.
+#If it succeeds, it will enter the main working loop forever. It will only return if an error occurs and return True to report that it was working with the correct device.
+def tryUsingPort(port):
+  try:
+    if device.connect(port):
+      work()  #Success, enter main loop
+      device.disconnect()
+      return True
+  except SerialException as e:
+    print("Serial error: ", e)
+  except:
+    #Something entirely unexpected happened. We will catch it nevertheless, so the device keeps working as this process will probably run in the background unsupervised. But we need to print a proper stacktrace, so we can debug the problem.
+    if DEBUG:
+      print(traceback.format_exc())
+      print("Error: ", sys.exc_info()[0])
+  return False
+
 #### main 
 
 sg.theme('LightGrey1')
+psg.theme('LightGrey1')
 config = loadConfig()
 
 # simple tray icon and menu
 menu_def = ['BLANK', ['&Settings', '---','E&xit']]
 tray = sg.SystemTray(menu=menu_def, filename=r'default_icon.ico')
 
-# Start the 'run' method in a daemonized thread.
-t = threading.Thread(target=windowChangeEventListener)
-t.setDaemon(True)
-t.start()
+# Instantiate the device
+device = Device()
+device.debug = DEBUG
 
-# Event Loop to process "events"
-while True:
-  menu_item = tray.read()
-  print(menu_item)
-  if menu_item == 'Exit':
-    tray.close()
-    break
-  elif menu_item == 'Settings':
-    settingsWindow(config)
-    config = loadConfig()
-    print("settings done")
+for port in serial.tools.list_ports.comports(): #Iterate over all serial ports
+  if port.vid != VID or port.pid != PID:
+    continue                                #Skip if vendor or product ID do not match
+  if tryUsingPort(port.device):             #Try connecting to this device
+    break                                   #Connection was successful and inkkeys was found. If we reach this point, we do not need to search on another port. We got disconnected or some other kind of error, so skip the rest of the port list and start over.
+
+
---
+++ modes.py
@@ -0,0 +1,249 @@
+from PIL import Image, ImageDraw, ImageFont
+
+class ModeApplication:
+  jogFunction = ""    #Keeps track of the currently selected function of the jog dial
+
+  def activate(self, device, exeName, appConfig):
+    device.sendTextFor("title", exeName, inverted=True)  #Title
+
+    #Button2 (top left)
+    device.sendIconFor(2, "icons/" + appConfig['button1']['icon'])
+    #device.assignKey(KeyCode.SW2_PRESS, [event(DeviceCode.KEYBOARD, KeyboardKeycode.KEY_LEFT_ALT, ActionCode.PRESS), event(DeviceCode.KEYBOARD, KeyboardKeycode.KEY_B), event(DeviceCode.KEYBOARD, KeyboardKeycode.KEY_LEFT_ALT, ActionCode.RELEASE), event(DeviceCode.KEYBOARD, KeyboardKeycode.KEY_Z)]) #Cut to selection (this shortcut appears to be language dependent, so you will probably need to change it)
+    #device.assignKey(KeyCode.SW2_RELEASE, [])
+
+    #Button3 (left, second from top)
+    device.sendIconFor(3, "icons/" + appConfig['button2']['icon'])
+    #device.assignKey(KeyCode.SW3_PRESS, [event(DeviceCode.KEYBOARD, KeyboardKeycode.KEY_LEFT_ALT, ActionCode.PRESS), event(DeviceCode.KEYBOARD, KeyboardKeycode.KEY_B), event(DeviceCode.KEYBOARD, KeyboardKeycode.KEY_LEFT_ALT, ActionCode.RELEASE), event(DeviceCode.KEYBOARD, KeyboardKeycode.KEY_I)]) #Cut to content (this shortcut appears to be language dependent, so you will probably need to change it)
+    #device.assignKey(KeyCode.SW3_RELEASE, [])
+
+    #Button4 (left, third from top)
+    device.sendIconFor(4, "icons/" + appConfig['button3']['icon'])
+    #device.assignKey(KeyCode.SW4_PRESS, [event(DeviceCode.KEYBOARD, KeyboardKeycode.KEY_LEFT_ALT, ActionCode.PRESS), event(DeviceCode.KEYBOARD, KeyboardKeycode.KEY_B), event(DeviceCode.KEYBOARD, KeyboardKeycode.KEY_LEFT_ALT, ActionCode.RELEASE), event(DeviceCode.KEYBOARD, KeyboardKeycode.KEY_L)]) #Canvas size (this shortcut appears to be language
+    #device.assignKey(KeyCode.SW4_RELEASE, [])
+
+    #Button5 (bottom left)
+    device.sendIconFor(5, "icons/" + appConfig['button4']['icon'])
+    #device.assignKey(KeyCode.SW5_PRESS, [event(DeviceCode.KEYBOARD, KeyboardKeycode.KEY_LEFT_ALT, ActionCode.PRESS), event(DeviceCode.KEYBOARD, KeyboardKeycode.KEY_B), event(DeviceCode.KEYBOARD, KeyboardKeycode.KEY_LEFT_ALT, ActionCode.RELEASE), event(DeviceCode.KEYBOARD, KeyboardKeycode.KEY_S)]) #Resize (this shortcut appears to be language
+    #device.assignKey(KeyCode.SW5_RELEASE, [])
+
+    #Button6 (top right)
+    device.sendIconFor(6, "icons/" + appConfig['button5']['icon'])
+    #device.assignKey(KeyCode.SW6_PRESS, [event(DeviceCode.KEYBOARD, KeyboardKeycode.KEY_LEFT_CTRL, ActionCode.PRESS), event(DeviceCode.KEYBOARD, KeyboardKeycode.KEY_LEFT_SHIFT, ActionCode.PRESS), event(DeviceCode.KEYBOARD, KeyboardKeycode.KEY_V), event(DeviceCode.KEYBOARD, KeyboardKeycode.KEY_LEFT_CTRL, ActionCode.RELEASE), event(DeviceCode.KEYBOARD, KeyboardKeycode.KEY_LEFT_SHIFT, ActionCode.RELEASE)]) #Paste as new image
+    #device.assignKey(KeyCode.SW6_RELEASE, [])
+
+    #Button7 (right, second from top)
+    device.sendIconFor(7, "icons/" + appConfig['button6']['icon'])
+    #device.assignKey(KeyCode.SW7_PRESS, [event(DeviceCode.KEYBOARD, KeyboardKeycode.KEY_LEFT_CTRL, ActionCode.PRESS), event(DeviceCode.KEYBOARD, KeyboardKeycode.KEY_LEFT_SHIFT, ActionCode.PRESS), event(DeviceCode.KEYBOARD, KeyboardKeycode.KEY_N), event(DeviceCode.KEYBOARD, KeyboardKeycode.KEY_LEFT_CTRL, ActionCode.RELEASE), event(DeviceCode.KEYBOARD, KeyboardKeycode.KEY_LEFT_SHIFT, ActionCode.RELEASE)]) #New layer
+    #device.assignKey(KeyCode.SW7_RELEASE, [])
+
+    #Button8 (right, third from top)
+    device.sendIconFor(8, "icons/" + appConfig['button7']['icon'])
+    #device.assignKey(KeyCode.SW8_PRESS, [event(DeviceCode.KEYBOARD, KeyboardKeycode.KEY_LEFT_CTRL, ActionCode.PRESS), event(DeviceCode.KEYBOARD, KeyboardKeycode.KEY_LEFT_SHIFT, ActionCode.PRESS), event(DeviceCode.KEYBOARD, KeyboardKeycode.KEY_J), event(DeviceCode.KEYBOARD, KeyboardKeycode.KEY_LEFT_CTRL, ActionCode.RELEASE), event(DeviceCode.KEYBOARD, KeyboardKeycode.KEY_LEFT_SHIFT, ActionCode.RELEASE)]) #Zom to fill screen
+    #device.assignKey(KeyCode.SW8_RELEASE, [])
+
+    #Button9 (bottom right)
+    device.sendIconFor(9, "icons/" + appConfig['button1']['icon'])
+    #device.assignKey(KeyCode.SW9_PRESS, []) #Not used, set to nothing.
+    #device.assignKey(KeyCode.SW9_RELEASE, [])
+
+    self.jogFunction = ""
+
+    #This toggles the jog function and sets up key assignments and the label for the jog dial. It calls "updateDiplay()" if update is not explicitly set to False (for example if you need to update more parts of the display before updating it.)
+    def toggleJogFunction(update=True):
+      return
+      # if self.jogFunction == "size":  #Tool opacity in GIMP
+      #   device.clearCallback(KeyCode.JOG)
+      #   device.sendTextFor(1, "Tool opacity")
+      #   device.assignKey(KeyCode.JOG_CW, [event(DeviceCode.KEYBOARD, KeyboardKeycode.KEY_LEFT_SHIFT, ActionCode.PRESS), event(DeviceCode.KEYBOARD, KeyboardKeycode.KEY_COMMA), event(DeviceCode.KEYBOARD, KeyboardKeycode.KEY_LEFT_SHIFT, ActionCode.RELEASE)])
+      #   device.assignKey(KeyCode.JOG_CCW, [event(DeviceCode.KEYBOARD, KeyboardKeycode.KEY_LEFT_SHIFT, ActionCode.PRESS), event(DeviceCode.KEYBOARD, KeyboardKeycode.KEY_PERIOD), event(DeviceCode.KEYBOARD, KeyboardKeycode.KEY_LEFT_SHIFT, ActionCode.RELEASE)])
+      #   self.jogFunction = "opacity"
+      #   if update:
+      #     device.updateDisplay()
+      # else:                            #Tool size in GIMP
+      #   device.clearCallback(KeyCode.JOG)
+      #   device.sendTextFor(1, "Tool size")
+      #   device.assignKey(KeyCode.JOG_CW, [event(DeviceCode.KEYBOARD, KeyboardKeycode.KEY_LEFT_BRACE)])
+      #   device.assignKey(KeyCode.JOG_CCW, [event(DeviceCode.KEYBOARD, KeyboardKeycode.KEY_RIGHT_BRACE)])
+      #   self.jogFunction = "size"
+      #   if update:
+      #     device.updateDisplay()
+
+    #Button 1 / jog dial press
+    #device.registerCallback(toggleJogFunction, KeyCode.JOG_PRESS) #Call "toggleJogFunction" if the dial is pressed
+    #device.assignKey(KeyCode.SW1_PRESS, [])                       #We do not send a key stroke when the dial is pressed, instead we use the callback.
+    #device.assignKey(KeyCode.SW1_RELEASE, [])                     #We still need to overwrite the assignment to clear previously set assignments.
+    #toggleJogFunction(False)    #We call toggleJogFunction to initially set the label and assignment
+    device.updateDisplay()      #Everything has been sent to the display. Time to refresh it.
+
+  def poll(self, device):
+    return False #Nothing to poll
+
+  def animate(self, device):
+    device.fadeLeds() #No LED animation is used in this mode, but we call "fadeLeds" anyway to fade colors that have been set in another mode before switching
+
+  def deactivate(self, device):
+    device.clearCallbacks() #Remove our callbacks if we switch to a different mode
+
+
+############## This mode is used as a fallback and a much more complex example than Gimp. It also uses a switchable Jog dial,
+## Fallback ## but most of its functions give a feedback via LED. Also, we use MQTT (via a separately defined class) to get
+############## data from a CO2 sensor and control a light (both including feedback)
+
+class ModeFallback:
+  jogFunction = ""    #Keeps track of the currently selected function of the jog dial
+  lightState = None   #Current state of the lights in my office. (Keeping track to know when to update the screen)
+  demoActive = False  #We have a demo button and this keeps track whether the demo mode is active, so we know when to update the screen
+
+  def activate(self, device):
+    device.sendTextFor("title", "Default", inverted=True) #Title
+
+    ### Buttons 2, 3, 6 and 7 are media controls ###
+    device.sendIconFor(2, "icons/play.png", centered=(not self.demoActive))
+    device.assignKey(KeyCode.SW2_PRESS, [event(DeviceCode.CONSUMER, ConsumerKeycode.MEDIA_PLAY_PAUSE, ActionCode.PRESS)])
+    device.assignKey(KeyCode.SW2_RELEASE, [event(DeviceCode.CONSUMER, ConsumerKeycode.MEDIA_PLAY_PAUSE, ActionCode.RELEASE)])
+    device.sendIconFor(3, "icons/skip-start.png", centered=(not self.demoActive))
+    device.assignKey(KeyCode.SW3_PRESS, [event(DeviceCode.CONSUMER, ConsumerKeycode.MEDIA_PREV, ActionCode.PRESS)])
+    device.assignKey(KeyCode.SW3_RELEASE, [event(DeviceCode.CONSUMER, ConsumerKeycode.MEDIA_PREV, ActionCode.RELEASE)])
+
+    device.sendIconFor(6, "icons/stop.png", centered=(not self.demoActive))
+    device.assignKey(KeyCode.SW6_PRESS, [event(DeviceCode.CONSUMER, ConsumerKeycode.MEDIA_STOP, ActionCode.PRESS)])
+    device.assignKey(KeyCode.SW6_RELEASE, [event(DeviceCode.CONSUMER, ConsumerKeycode.MEDIA_STOP, ActionCode.RELEASE)])
+    device.sendIconFor(7, "icons/skip-end.png", centered=(not self.demoActive))
+    device.assignKey(KeyCode.SW7_PRESS, [event(DeviceCode.CONSUMER, ConsumerKeycode.MEDIA_NEXT, ActionCode.PRESS)])
+    device.assignKey(KeyCode.SW7_RELEASE, [event(DeviceCode.CONSUMER, ConsumerKeycode.MEDIA_NEXT, ActionCode.RELEASE)])
+
+    ### Buttons 5 and 9 are shortcuts to applications ###
+    device.sendIconFor(5, "icons/envelope.png", centered=(not self.demoActive))
+    device.assignKey(KeyCode.SW5_PRESS, [event(DeviceCode.CONSUMER, ConsumerKeycode.CONSUMER_EMAIL_READER, ActionCode.PRESS)])
+    device.assignKey(KeyCode.SW5_RELEASE, [event(DeviceCode.CONSUMER, ConsumerKeycode.CONSUMER_EMAIL_READER, ActionCode.RELEASE)])
+    device.sendIconFor(9, "icons/calculator.png", centered=(not self.demoActive))
+    device.assignKey(KeyCode.SW9_PRESS, [event(DeviceCode.CONSUMER, ConsumerKeycode.CONSUMER_CALCULATOR, ActionCode.PRESS)])
+    device.assignKey(KeyCode.SW9_RELEASE, [event(DeviceCode.CONSUMER, ConsumerKeycode.CONSUMER_CALCULATOR, ActionCode.RELEASE)])
+
+    ### Button 4 controls the light in my office and displays its state ###
+    def toggleLight():
+      target = not self.lightState
+      self.mqtt.setLight(target)
+      self.lightState = target
+      self.showLightState(device)
+
+      self.lightState = self.mqtt.getLight
+      self.showLightState(device, False)
+
+      device.assignKey(KeyCode.SW4_PRESS, [])
+      device.assignKey(KeyCode.SW4_RELEASE, [])
+      device.registerCallback(toggleLight, KeyCode.SW4_PRESS)
+
+    ### Button 8 set display and LEDs to a demo state (only used for videos and pictures of the thing)
+    def toggleDemo():
+      if self.demoActive:
+        self.demoActive = False
+        img = Image.new("1", (device.dispW, device.dispH), color=1)
+        device.sendImage(0, 0, img)
+        self.activate(device) #Recreate the screen content after the demo
+      else:
+        self.demoActive = True
+        self.activate(device) #Recreate the screen because with demo active, the buttons will align differently to give room for "there.oughta.be"
+        text = "there.oughta.be/a/macro-keyboard"
+        font = ImageFont.truetype("arial.ttf", 17)
+        w, h = font.getsize(text);
+        x = (device.dispW-h)//2
+        x8 = floor(x / 8) * 8 #needs to be a multiple of 8
+        h8 = ceil((h + x - x8) / 8) * 8 #needs to be a multiple of 8
+        img = Image.new("1", (w, h8), color=1)
+        d = ImageDraw.Draw(img)
+        d.text((0, x-x8), text, font=font, fill=0)
+        device.sendImage(x8, (device.dispH-w)//2, img.transpose(Image.ROTATE_90))
+        device.updateDisplay(True)
+
+    device.registerCallback(toggleDemo, KeyCode.SW8_PRESS)
+    device.sendIconFor(8, "icons/emoji-sunglasses.png", centered=(not self.demoActive))
+    device.assignKey(KeyCode.SW8_PRESS, [])
+    device.assignKey(KeyCode.SW8_RELEASE, [])
+
+    ### The jog wheel can be pressed to switch between three functions: Volume control, mouse wheel, arrow keys left/right ###
+
+    def showVolume(n):
+      # with pulsectl.Pulse('inkkeys') as pulse:
+      #     sinkList = pulse.sink_list()
+      #     name = pulse.server_info().default_sink_name
+      #     for sink in sinkList:
+      #         if sink.name == name:
+      #             vol = sink.volume.value_flat
+      vol = 0
+      off = 0x00ff00
+      on = 0xff0000
+      leds = [on if vol > i/(device.nLeds-1) else off for i in range(device.nLeds)]
+      device.setLeds(leds)
+
+    self.jogFunction = ""
+
+    def toggleJogFunction(update=True):
+      if self.jogFunction == "wheel":
+        device.clearCallback(KeyCode.JOG)
+        device.sendTextFor(1, "Arrow Keys")
+        device.assignKey(KeyCode.JOG_CW, [event(DeviceCode.KEYBOARD, KeyboardKeycode.KEY_RIGHT)])
+        device.assignKey(KeyCode.JOG_CCW, [event(DeviceCode.KEYBOARD, KeyboardKeycode.KEY_LEFT)])
+        self.jogFunction = "arrow"
+        if update:
+          device.updateDisplay()
+      elif self.jogFunction == "arrow":
+        device.sendTextFor(1, "Volume")
+        device.registerCallback(showVolume, KeyCode.JOG)
+        device.assignKey(KeyCode.JOG_CW, [event(DeviceCode.CONSUMER, ConsumerKeycode.MEDIA_VOL_UP)])
+        device.assignKey(KeyCode.JOG_CCW, [event(DeviceCode.CONSUMER, ConsumerKeycode.MEDIA_VOL_DOWN)])
+        self.jogFunction = "volume"
+        if update:
+          device.updateDisplay()
+      else:
+        device.clearCallback(KeyCode.JOG)
+        device.sendTextFor(1, "Mouse Wheel")
+        device.assignKey(KeyCode.JOG_CW, [event(DeviceCode.MOUSE, MouseAxisCode.MOUSE_WHEEL, 1)])
+        device.assignKey(KeyCode.JOG_CCW, [event(DeviceCode.MOUSE, MouseAxisCode.MOUSE_WHEEL, -1)])
+        self.jogFunction = "wheel"
+        if update:
+          device.updateDisplay()
+
+    device.registerCallback(toggleJogFunction, KeyCode.JOG_PRESS)
+    device.assignKey(KeyCode.SW1_PRESS, [])
+    device.assignKey(KeyCode.SW1_RELEASE, [])
+    toggleJogFunction(False)
+
+    ### All set, let's update the display ###
+    device.updateDisplay()
+
+  def poll(self, device):
+    if not self.demoActive:
+      co2 = self.mqtt.getCO2()
+      if co2 != None and co2 > 1000:
+        leds = [0x0000ff for i in range(device.nLeds)]
+        device.setLeds(leds)
+      light = self.mqtt.getLight()
+      if light != self.lightState:
+        self.lightState = light
+        self.showLightState(device)
+    return 10 #Since we only retrieve the current state from the Mqtt class, the 10 seconds do not really control how often the value is querries but how often we react to it. For the CO2 warning, this is a not too intrusive blue flash every 10 seconds and a 10 second delay to update the light state if it has been switched from somewhere else (rare) seems reasonable, too.
+
+  #Called to update the icon of button 4, showing the state of the office light (as if I couldn't see it in the real room, but it is a nice touch to update the display accordingly)
+  def showLightState(self, device, update=True):
+    if self.lightState:
+      device.sendIconFor(4, "icons/lightbulb.png", centered=(not self.demoActive))
+    else:
+      device.sendIconFor(4, "icons/lightbulb-off.png", centered=(not self.demoActive))
+    if update:
+      device.updateDisplay()
+
+  def animate(self, device):
+    if self.demoActive: #In demo mode, we animate the LEDs here
+
+      def rgbTupleToInt(rgb):
+        return (int(rgb[0]*255) << 16) | (int(rgb[1]*255) << 8) | int(rgb[2]*255)
+
+      t = time.time()
+      leds = [rgbTupleToInt(hsv_to_rgb(t + i/device.nLeds, 1, 1)) for i in range(device.nLeds)]
+      device.setLeds(leds)
+    else:               #If not in demo mode, we call "fadeLeds" to create a fade animation for any color set anywhere in this mode
+      device.fadeLeds()
+
+  def deactivate(self, device):
+    device.clearCallbacks() #Clear our callbacks if we switch to a different mode
